94c4b6615657b2181af64692dd904d3c397228a4
Amna Bajwa <93836658+Amna20100324@users.noreply.github.com>
2022-04-23 11:44:31 +0100

    Add files via upload

---
+++ PRO-C48-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "DUMBBELL";
+const accessCode2 = "KEYBOARD";
+const accessCode3 = "CLOCK";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("M U D B L E L B", 100,50)
+    fill("lightblue")
+    text("Hint: Used to get muscles !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A K B R D E O Y", 700,150)
+    fill("lightblue")
+    text("Hint: You type with it !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("L C K C O ", 100,250)
+    fill("lightblue")
+    text("Hint: Used to tell the time !!", 100,270)
+
+}
